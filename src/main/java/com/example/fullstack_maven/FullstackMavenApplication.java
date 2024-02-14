9d66c8e4bd88542ec3246ac9849b486ee1e59a51
gabrielubiale <gabrielfubiale@gmail.com>
2024-02-14 19:04:33 -0300

    first commit

---
+++ src/main/java/com/example/fullstack_maven/FullstackMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstack_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackMavenApplication.class, args);
+	}
+
+}
